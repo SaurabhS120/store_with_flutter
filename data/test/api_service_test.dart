b11ae12fd2757d05466ed7b54d2964a1a711162e
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-06-04 11:36:26 +0530

    Data layer mocking

---
+++ data/test/api_service_test.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:data/api_service.dart';
+
+@GenerateNiceMocks([MockSpec<ApiService>()])
+import 'api_service_test.mocks.dart';
+
--- data/test/repo_impl/product_repo_impl_test.dart
+++ data/test/repo_impl/product_repo_impl_test.dart
@@ -1,6 +1,31 @@
+import 'package:data/api_service.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:data/repo_impl/products_repo_impl.dart';
 
+import '../api_service_test.mocks.dart';
 @GenerateNiceMocks([MockSpec<ProductsRepoImpl>()])
+import 'product_repo_impl_test.mocks.dart';
+
 void main(){
+  late ProductsRepoImpl repo;
+  late ApiService apiService;
+
+  setUp((){
+    repo = MockProductsRepoImpl();
+    apiService = MockApiService();
+  });
+
+  test('getProducts', () async {
+    when(repo.getProducts()).thenAnswer((_) async => []);
+    final result = await repo.getProducts();
+    expect(result, []);
+  });
+
+  test('getProducts from mock rest api', () async {
+    when(apiService.getProducts()).thenAnswer((_) async => []);
+    final result = await apiService.getProducts();
+    expect(result, []);
+  });
 }
