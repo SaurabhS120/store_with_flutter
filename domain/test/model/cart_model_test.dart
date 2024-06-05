80c5a59363511b4558cba8f1c8cf19e931c370d9
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-06-05 15:53:15 +0530

    Equatable test

--- domain/test/model/cart_model_test.dart
+++ domain/test/model/cart_model_test.dart
@@ -1,16 +1,23 @@
 import 'package:domain/model/cart_model.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
   group("ProductCartModel model equality test", (){
+    const ProductCartModel productCartModel = ProductCartModel(productId: 1, quantity: 1);
+    test("Subclass of Equatable", (){
+      expect(productCartModel, isA<Equatable>());
+    });
     test("equality", (){
-      const ProductCartModel productCartModel = ProductCartModel(productId: 1, quantity: 1);
       expect(productCartModel, const ProductCartModel(productId: 1, quantity: 1));
     });
   });
   group("CartModel equality test", (){
+    final CartModel cartModel = CartModel(id: 1, userId: 1, date: DateTime(2020, 3, 2), products: const []);
+    test("Subclass of Equatable", (){
+      expect(cartModel, isA<Equatable>());
+    });
     test("equality", (){
-      final CartModel cartModel = CartModel(id: 1, userId: 1, date: DateTime(2020, 3, 2), products: const []);
       expect(cartModel, CartModel(id: 1, userId: 1, date: DateTime(2020, 3, 2), products: const []));
     });
   });
